863c1a9d13c600304dda1fc53f3d712fdae41b60
Jeff Tanner, Tango Card <jeff@tangocard.com>
2012-07-30 23:23:07 -0700

    TangoCard_DotNet_SDK dev - Help docs

--- TangoCard.Sdk/Properties/AssemblyInfo.cs
+++ TangoCard.Sdk/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TangoCard_DotNet_SDK")]
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("TangoCard_DotNet_SDK")]
 [assembly: AssemblyCopyright("Copyright © Tangocard 2012")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,4 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
