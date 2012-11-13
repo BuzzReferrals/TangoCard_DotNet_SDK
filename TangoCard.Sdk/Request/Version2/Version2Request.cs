4ac97c8a4a68cad50f14a9ed96c69fa694d5c770
Jeff Tanner, Tango Card <jeff@tangocard.com>
2012-11-13 10:11:59 -0800

    Comment cleanup.

--- TangoCard.Sdk/Request/Version2/Version2Request.cs
+++ TangoCard.Sdk/Request/Version2/Version2Request.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿//  
+//  Version2Request.cs
+//  TangoCard_DotNet_SDK
+//  
+//  Copyright (c) 2012 Tango Card, Inc
+//  All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions: 
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software. 
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Runtime.Serialization;
 using TangoCard.Sdk.Service;
 
 namespace TangoCard.Sdk.Request.Version2
 {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>   Version 2 request. </summary>
+    ///
+    /// <seealso cref="TangoCard.Sdk.Request.BaseRequest"/>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     [DataContract]
     public abstract class Version2Request : BaseRequest
     {
@@ -44,21 +75,30 @@ namespace TangoCard.Sdk.Request.Version2
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Gets the username. </summary>
+        /// <summary>   Gets or sets the username. </summary>
         ///
         /// <value> The username. </value>
         ////////////////////////////////////////////////////////////////////////////////////////////////////
+
         [DataMember(Name = "username", IsRequired = true)]
         public string Username { get; set; }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Gets the password. </summary>
+        /// <summary>   Gets or sets the password. </summary>
         ///
         /// <value> The password. </value>
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         [DataMember(Name = "password", IsRequired = true)]
         public string Password { get; set; }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets the request controller. </summary>
+        ///
+        /// <seealso cref="TangoCard.Sdk.Request.BaseRequest.RequestController"/>
+        ///
+        /// ### <value> The request controller. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         public override string RequestController
         {
